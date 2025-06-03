7ff7c73c035af609026f21b3b8b3b7289dee598c
SudarshanaRao <pandu.sudha2003@gmail.com>
2025-06-03 11:45:48 +0530

    Updated some files for vercel

--- client/src/store/shop/order-slice/index.js
+++ client/src/store/shop/order-slice/index.js
@@ -14,12 +14,10 @@ const initialState = {
 export const createNewOrder = createAsyncThunk(
   "order/createNewOrder",
   async (orderData) => {
-    
     const response = await axios.post(
       "http://localhost:5000/api/shop/order/create",
       orderData
     );
-    
 
     return {
       order: {
