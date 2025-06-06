df26daabd77ead84b09fe87f3b55cb4f599997b1
SudarshanaRao <pandu.sudha2003@gmail.com>
2025-06-06 13:12:41 +0530

    Checking cors blocking

--- server/server.js
+++ server/server.js
@@ -22,7 +22,10 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 //create a separate file for this and then import/use that file here
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log("Atlas MongoDB connected Successfully"))
   .catch((error) => console.log(error));
 
