8e08bc90f2a2c43b04b9956eb569f6a21f7d7501
SudarshanaRao <pandu.sudha2003@gmail.com>
2025-06-06 10:18:55 +0530

    Checking cors blocking

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
@@ -45,6 +45,7 @@ app.use(
     credentials: true,
   })
 );
+app.options('*', cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the E-Commerce API!");
